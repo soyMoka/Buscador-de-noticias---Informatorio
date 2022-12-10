8ffac1651d2a43c4c08e59b5b437b39dcb56007c
soyMoka <d15.cejas.matias@gmail.com>
2022-12-10 00:20:01 -0300

    paginacion de 10 en 10 lista

--- src/services/noticias-api.js
+++ src/services/noticias-api.js
@@ -1,9 +1,10 @@
-const LANG = 'es'
-const API_KEY = '30ad71315c9d4cb3bd514a6c2701c648'
+const LANG = 'es';
+const API_KEY = '30ad71315c9d4cb3bd514a6c2701c648';
 const OMDB_API = 'https://newsapi.org/v2/everything';
+const NOTICIAS_POR_PAGINA = 10;
 
 export const getListadoNoticias = async (criterioBusqueda, paginaActual) => {
-    const respuesta = await fetch(`${OMDB_API}?q=${criterioBusqueda}&apiKey=${API_KEY}&language=${LANG}&page=${paginaActual}&sortBy=publishedAt`);
+    const respuesta = await fetch(`${OMDB_API}?q=${criterioBusqueda}&apiKey=${API_KEY}&language=${LANG}&page=${paginaActual}&sortBy=publishedAt&pageSize=${NOTICIAS_POR_PAGINA}`);
     const noticias = await respuesta.json();
     
     console.log('servicio .json')
